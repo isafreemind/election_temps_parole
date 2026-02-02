3f5fccbe119e2539a1aa575026b45fcb4b0b34f5
isafreemind <isafreemind@gmail.com>
2026-02-02 13:34:36 +0100

    Add data

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/election_temps_parole/',
+  base: '/election-temps-parole/',
   build: {
     outDir: '../docs',
     emptyOutDir: true,
